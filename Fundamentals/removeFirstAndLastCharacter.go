45a99cdc2e453a48b53e42aff3f776d54f89602f
Yasin Shuman <email@yasin.io>
2019-01-22 13:46:23 -0800

    finished removeFirstAndLastCharacter.go

---
+++ Fundamentals/removeFirstAndLastCharacter.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+ */
+
+func RemoveChar(word string) string {
+	return word[1 : len(word)-1]
+
+}
+
+func main() {
+	fmt.Println("the word is apple and should print ppl: ")
+	fmt.Println(RemoveChar("apple"))
+
+}
