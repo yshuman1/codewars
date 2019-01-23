8fc1c29bddef414f72305ad7fa752656fc8b2374
Yasin Shuman <email@yasin.io>
2019-01-22 19:12:20 -0800

    finished isItPalindrome.go

---
+++ Fundamentals/isItAPalindrome.go
@@ -0,0 +1,25 @@
+// Write function isPalindrome that checks if a given string (case insensitive) is a palindrom.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func IsPalindrome(str string) bool {
+	str =strings.ToLower(str)
+	for i:=0; i < len(str)/2; i++{
+		if str[i] != str[len(str)-i-1]{
+			return false
+		}
+	}
+	return true
+}
+
+func main(){
+	fmt.Println(IsPalindrome("a"))
+	fmt.Println(IsPalindrome("aba"))
+	fmt.Println(IsPalindrome("Abba"))
+	fmt.Println(IsPalindrome("hello"))
+
+}
