e7996cbe091881032bad21a96e1873e896c8d1fe
Yasin Shuman <email@yasin.io>
2019-01-22 19:00:21 -0800

    completed evenOrOdd.go

---
+++ Fundamentals/evenOrOdd.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Create a function (or write a script in Shell) that takes an integer as an argument and
+// returns "Even" for even numbers or "Odd" for odd numbers.
+
+func EvenOrOdd(number int) string {
+	// your code here
+	if number%2 != 0 {
+		return "Odd"
+	} else {
+		return "Even"
+	}
+}
+
+func main() {
+	fmt.Println("this should return odd: ", EvenOrOdd(5))
+}
